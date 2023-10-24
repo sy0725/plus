efb6ce68000a984426c32f9064d104d13e4f4b65
Sunyong Jeon <jeon@Sunyongui-MacBookAir.local>
2023-10-24 18:00:51 +0900

    1024 수업 내용

--- ch01/ex01-20.js
+++ ch01/ex01-20.js
@@ -1,8 +1,8 @@
 var user = {
-  userName: '김철수',
+  userName: "김철수",
   age: 30,
-  phone: '0102223333',
-  address: '서울특별시 종로구 종로3길 17 D타워, 16-17층 (주)멋쟁이사자처럼'
+  phone: "0102223333",
+  address: "서울특별시 종로구 종로3길 17 D타워, 16-17층 (주)멋쟁이사자처럼",
 };
 
 /* 다음처럼 출력할 수 있도록 info를 작성하시오.
@@ -11,7 +11,34 @@ var user = {
 전화번호: 0102223333
 주소: 서울시
 */
-var info = '';
+var info =
+  user.userName +
+  "님의 정보입니다.\n나이" +
+  user.age +
+  "\n전화번호" +
+  user.phone +
+  "\n주소:" +
+  user.address;
+
+var info = `${user.userName}님의 정보입니다.
+나이 : ${user.age}
+전화번호 : ${user.phone}
+주소 : ${user.address}`;
 
 console.log(info);
 
+// Tagged templage literal
+function userInfo(strings, userName, age, phone, address) {
+  console.log(strings);
+  console.log(userName);
+  console.log(age);
+  console.log(phone);
+  console.log(address);
+}
+
+var kim = userInfo`
+${user.userName}님의 정보입니다.
+나이 : ${user.age}
+전화번호 : ${user.phone}
+주소 : ${user.address}
+`;
