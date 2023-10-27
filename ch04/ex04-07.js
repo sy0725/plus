808f14e6024fc9dc1f6091c4033f9f1bd7d1c385
Jeon SunYong <sunyong0725@naver.com>
2023-10-27 13:10:07 +0900

    1027 수업내용 추가

--- ch04/ex04-07.js
+++ ch04/ex04-07.js
@@ -1,4 +1,22 @@
-var alert = '출입 금지!';
-console.log(alert);
+// var alert = '출입 금지!';
+// console.log(alert);
 
-alert('작업 완료.');
+function fn() {
+  var alert = "출입 금지!";
+  console.log(alert);
+}
+fn();
+
+(function () {
+  var alert = "출입 금지!";
+  console.log(alert);
+})();
+
+function fn2(alert) {
+  console.log(alert);
+}
+fn2("출입금지")((alert) => {
+  console.log(alert);
+})("출입금지");
+
+alert("작업 완료.");
